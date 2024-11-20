9550aa90c89a8a6341e34ba778e36b693fff92a3
tranxuanbach0 <trantiger706@gmail.com>
2024-11-20 21:23:09 +0700

    session02 2

---
+++ session02 2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Khai bao va khoi tao hai bien so nguyen 
+    int num1 = 1; // Bien so nguyen thu nhat 
+    int num2 = 1; // Bien so nguyen thu hai 
+
+    // Tinh tong cua hai so roi luu ket qua va sum 
+    int sum = num1 + num2;
+
+    // In ket qua ra man hinh 
+    printf("Tong cua %d và %d là: %d\n", num1, num2, sum);
+
+    return 0;
+}
