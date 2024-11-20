7ea5f3b8797695924a09ed9e0e440e62514d1454
tranxuanbach0 <trantiger706@gmail.com>
2024-11-20 21:15:35 +0700

    session02 1

---
+++ session02 1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+  // int la kieu du lieu so nguyen 
+    int a = 1;
+  // fload va double la kieu du lieu so thap phan nhung double co kich thuoc luu tru lon hon
+    float sum = 3.3;
+    double b = 3.3333333333;
+  // char la kieu du lieu ki tu
+     char c  = 'A';
+ }
