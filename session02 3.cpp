f197f80a2e3dca0068bc3f2889ad7b60db66844e
tranxuanbach0 <trantiger706@gmail.com>
2024-11-20 21:29:43 +0700

    session02 3

---
+++ session02 3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Khai bao va khoi tao hai bien so nguyen 
+    int num1 = 1; // Bien so nguyen thu nhat 
+    int num2 = 1; // Bien so nguyen thu hai 
+
+    // Tinh tong cua hai so roi luu ket qua va sum 
+    int sum = num1 + num2;   // tong 
+    int difference = num1 - num2;   // hieu 
+    int product = num1 * num2;      // tich
+    int quotient = num1 / num2; // thuong 
+    
+    return 0;
+}
